78a060f1ec7adf2198bf29f3509f234b350f5840
morizyun <admin@crownd.info>
2013-10-26 18:01:49 +0900

    chat => clockへの遷移 修正

--- public/js/chat_timer.js
+++ public/js/chat_timer.js
@@ -1,9 +1,26 @@
 $(function(){
     var PENQ={
         goToClock: function(opt){
+            var me=this;
+            params = me.getUrlParams();
+            room = params["room"];
+            if (room) {
+                url = "/clock?room=" + room;
+            } else {
+                url = "/clock";
+            }
             //return;
-            //location.href="/clock";
+            location.href = url;
         },
+        getUrlParams: function(){
+          var vars = new Object, params;
+          var temp_params = window.location.search.substring(1).split('&');
+          for(var i = 0; i <temp_params.length; i++) {
+            params = temp_params[i].split('=');
+            vars[params[0]] = params[1];
+          }
+          return vars;
+       },
         countdown: function(opt){
             var me=this;
             if(!opt){opt={};} if(!opt.id){opt.id='area';}
@@ -12,7 +29,7 @@ $(function(){
             if(!opt.hour){opt.hour=0;}
             var dt=new Date(opt.year, opt.month-1, opt.day, opt.hour, 0, 0); var dn=new Date();
             var d=dt-dn;
-            if(d<0){$('#'+opt.id).html(opt.already); return;}
+            if(d<0){$('#'+opt.id).html(opt.already); me.goToClock();}
             var dd=Math.floor(d/(1000*60*60*24));
             var hh=Math.floor((d-dd*1000*60*60*24)/(1000*60*60));
             var mm=Math.floor((d-(dd*1000*60*60*24)-(hh*1000*60*60))/(1000*60));
@@ -25,35 +42,7 @@ $(function(){
             x=x.replace('#m', edit(mm, opt)); x=x.replace('#s', edit(ss, opt));
             $('#'+opt.id).html(x);
             setTimeout(function(){me.countdown(opt);}, 1000);
-        },
-        scheduleTimer: function(opt){
-            var me=this;
-            if(!opt){opt={};} if(!opt.id){opt.id='area';}
-
-            hour = parseInt(opt.hour);
-            min = parseInt(opt.min);
-            if (hour == 9) {
-                me = 1
-            } else if (hour == 10) {
-                me = 2
-            } else if (hour == 11) {
-                me = 3
-            } else if (hour == 12) {
-                me.goToClock();
-            } else if (hour == 13) {
-                me = 4;
-            } else if (hour == 14) {
-                me = 5;
-            } else if (hour == 15) {
-                me = 6;
-            } else {
-                $('#'+opt.id).html(opt.already);
-                me.goToClock();
-            }
-            text = opt.format.replace('#st', num);
-            $('#'+opt.id).html(text);
-        },
-        close: {}
+        }
     }
 
     $(function(){
@@ -73,14 +62,6 @@ $(function(){
             format: '#m:#s',
             number: '#',
             already: '<span>今日の授業は終了しました。明日は9:00-スタートです。</span>' // ターゲットを経過した時の表示
-        });
-
-        PENQ.scheduleTimer({
-            id: 'schedule-title', // 表示場所
-            year: year, month: month, day: day, hour: hour, // ターゲット日
-            format: '#st時間目',
-            number: '#',
-            already: '本日の授業は終了しました。' // ターゲットを経過した時の表示
         });
     });
 });
